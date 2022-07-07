8d8ea72111a44d564d796f054179d2240f6e29ff
Harsh Dadhich <hdadhich01@gmail.com>
2022-07-07 16:16:20 -0700

    Remove comment

--- round_nutrition/__init__.py
+++ round_nutrition/__init__.py
@@ -3,8 +3,6 @@ from vitamin import Vitamin
 from mineral import Mineral
 from other import Other
 
-# test comment
-
 __version__ = "1.0.0"
 __sources__ = [
     "https://www.fda.gov/files/food/published/Food-Labeling-Guide-%28PDF%29.pdf",
