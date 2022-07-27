62ac27df19075bbd6a9cd486bc3be34f07956f63
Harsh Dadhich <hdadhich01@gmail.com>
2022-07-26 18:51:10 -0700

    Silence flake8's F401

--- round_nutrition/__init__.py
+++ round_nutrition/__init__.py
@@ -5,6 +5,7 @@ from .vitamin import Vitamin
 
 __version__ = "1.0.9"
 __doc__ = "Round nutritional values for federal compliance with FDA and NDC guidelines. More information available at https://github.com/hdadhich01/round-nutrition."
+__all__ = ["Main", "Mineral", "Other", "Vitamin"]
 __sources__ = [
     "https://www.fda.gov/files/food/published/Food-Labeling-Guide-%28PDF%29.pdf",
     "https://www.usdairy.com/~/media/usd/public/dmi-quick-reference-guide_nutrition-claims-for-dairy-products_2018.pdf",
